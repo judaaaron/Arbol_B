f2fa014902db76999cabf303561088264ebece0a
noheliaeuceda <noheliaeucedaf@gmail.com>
2020-12-06 20:52:01 -0600

    Menu incorporado

--- src/arbolb/ArbolB.java
+++ src/arbolb/ArbolB.java
@@ -7,10 +7,19 @@ public class ArbolB {
     static Scanner leer = new Scanner(System.in);
 
     public static void main(String[] args) {
-
-        TreeB tb = new TreeB(4);
+        int orden;
+        System.out.println("Ingrese el orden");
+        orden = leer.nextInt();
+        TreeB tb = new TreeB(orden);
         int n = 0;
-        for (int i = 1; i <= 16; i++) {
+        boolean flag = true;
+        System.out.println("Inserte más de "+orden+" numeros");
+        
+        while(flag){
+        // (int i = 1; i <= 16; i++) {
+            System.out.println("Ingrese el numero que desea insertar");
+            int i;
+            i = leer.nextInt();
             try {
 
                 n = i;
@@ -19,11 +28,17 @@ public class ArbolB {
                 break;
             }
             tb.insert(n);
+            System.out.println("Desea seguir insertando? \n1. Si \n2. No");
+            int res;
+            res = leer.nextInt();
+            if(res==2){
+                flag = false;
+            }
         }
         tb.imprimir_arbol(tb.raiz, 0);
-        NodoIndice ni = tb.B_Tree_Search(tb.getRaiz(), 8);
+        /*NodoIndice ni = tb.B_Tree_Search(tb.getRaiz(), 8);
         
-        System.out.println("Nodo: " + ni.getNodo() + "| Indice: " + ni.getIndice());
+        System.out.println("Nodo: " + ni.getNodo() + "| Indice: " + ni.getIndice());*/
 
     }
 
